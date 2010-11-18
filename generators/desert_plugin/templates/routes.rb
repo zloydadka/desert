7537001b54001aedd5595172c015e47ccd65bbfc
Alexandr Andrianov <alexandr.andrianov@rdtex.ru>
2010-11-18 17:22:15 +0300

    fix

--- generators/desert_plugin/templates/routes.rb
+++ generators/desert_plugin/templates/routes.rb
@@ -1,6 +1,6 @@
 # Add your custom routes here.  If in config/routes.rb you would 
 # add <tt>map.resources</tt>, here you would add just <tt>resources</tt>
 ActionController::Routing::Routes.add_routes do |map|
-  # resources :<%= plural_name %>
+  # map.resources :<%= plural_name %>
 end
 
